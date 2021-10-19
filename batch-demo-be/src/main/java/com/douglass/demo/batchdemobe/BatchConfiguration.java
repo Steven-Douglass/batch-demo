4ce55f4239830f0d7bdb4fb0eab30b283de1ce85
Steve940 <smd940@gmail.com>
2021-10-18 22:23:50 -0400

    Define item reader and sample data file

--- batch-demo-be/src/main/java/com/douglass/demo/batchdemobe/BatchConfiguration.java
+++ batch-demo-be/src/main/java/com/douglass/demo/batchdemobe/BatchConfiguration.java
@@ -5,8 +5,12 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 import com.douglass.demo.batchdemobe.model.ExportData;
 import com.douglass.demo.batchdemobe.model.ImportData;
@@ -17,6 +21,7 @@ public class BatchConfiguration {
 	private static final String JOB_NAME = "importDataJob";
 	private static final String STEP_NAME = "importDataStep";
 	private static final Integer AED_DATA_CHUNK_SIZE = 100;
+	private static final String[] IMPORT_DATA_PARAMETERS = {"color", "number"};
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -38,8 +43,22 @@ public class BatchConfiguration {
 	public Step step() {
 		return stepBuilderFactory.get(STEP_NAME)
 				.<ImportData, ExportData>chunk(AED_DATA_CHUNK_SIZE)
-				.reader(null)
+				.reader(reader())
 				.writer(null)
+				.build();
+	}
+	
+	@Bean
+	public FlatFileItemReader<ImportData> reader() {
+		BeanWrapperFieldSetMapper<ImportData> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+		beanWrapperFieldSetMapper.setTargetType(ImportData.class);
+
+		return new FlatFileItemReaderBuilder<ImportData>()
+				.name("importDataReader")
+				.resource(new ClassPathResource("sample-data.csv"))
+				.delimited()
+				.names(IMPORT_DATA_PARAMETERS)
+				.fieldSetMapper(beanWrapperFieldSetMapper)
 				.build();
 	}
 
