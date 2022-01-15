c5cc0b31dbfff4c1418f6fe7458cdfa1d23c5b81
Steven Douglass <smd940@gmail.com>
2022-01-15 15:58:05 -0500

    Adding api mapping for BatchRestController class

--- batch-demo-be/src/main/java/com/douglass/demo/batchdemobe/controller/BatchRestController.java
+++ batch-demo-be/src/main/java/com/douglass/demo/batchdemobe/controller/BatchRestController.java
@@ -1,11 +1,7 @@
 package com.douglass.demo.batchdemobe.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
@@ -17,11 +13,13 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglass.demo.batchdemobe.BatchConfiguration;
 
 @RestController
+@RequestMapping("/api")
 public class BatchRestController {
 
 	private final JobLauncher jobLauncher;
