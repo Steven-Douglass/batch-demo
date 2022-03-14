2b84fb878c22592435e9f8ec7acb23ab41584fb0
Steven Douglass <smd940@gmail.com>
2022-03-14 15:26:47 -0400

    Expanding build script

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 pwd;
 cd batch-demo-be;
 pwd;
-./mvnw clean install -DskipTests
+./mvnw clean install -DskipTests;
 cd ..;
 pwd;
 sudo apt update;
 sudo apt install -y docker.io;
 sudo systemctl enable docker --now;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+sudo chmod +x /usr/local/bin/docker-compose;
 docker-compose up --build;
