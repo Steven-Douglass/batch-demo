c259a2bd786acad8a0e7cd3d3d87553692774049
Steven Douglass <smd940@gmail.com>
2022-03-14 17:49:25 -0400

    Updating build script

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-pwd;
 cd batch-demo-be;
-pwd;
 ./mvnw clean install -DskipTests;
 cd ..;
-pwd;
-sudo apt update;
-sudo apt install -y docker.io;
-sudo systemctl enable docker --now;
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-sudo chmod +x /usr/local/bin/docker-compose;
+#sudo apt update;
+#sudo apt install -y docker.io;
+#sudo systemctl enable docker --now;
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+#sudo chmod +x /usr/local/bin/docker-compose;
 sudo docker-compose up --build;
